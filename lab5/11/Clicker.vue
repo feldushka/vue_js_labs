30a7f33439c5dd2ed7f39c74380b4db1521aa253
feldushka <mgjmb6w2jm@privaterelay.appleid.com>
2026-05-16 17:42:55 +0300

    Create Clicker.vue

---
+++ lab5/11/Clicker.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="clicker-card">
+    <span @click="handleClick" class="clickable-area">
+      <slot></slot>
+    </span>
+    
+    <p class="score">Очки: {{ count }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    required: true
+  }
+})
+
+const emit = defineEmits(['button-clicked'])
+
+const count = ref(0)
+
+const handleClick = () => {
+  count.value += props.amount
+  emit('button-clicked')
+}
+</script>
+
+<style scoped>
+.clicker-card {
+  border: 1px solid #ddd;
+  padding: 15px;
+  margin: 5px;
+  border-radius: 8px;
+  display: inline-block;
+  text-align: center;
+  background-color: #f9f9f9;
+}
+.clickable-area {
+  display: inline-block;
+  cursor: pointer;
+}
+.score {
+  margin-top: 10px;
+  font-weight: bold;
+}
+</style>
