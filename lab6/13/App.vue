a40d666c2d9805f61fc8d21262b645f7d3a77f61
feldushka <mgjmb6w2jm@privaterelay.appleid.com>
2026-05-16 15:58:02 +0300

    Create App.vue

---
+++ lab6/13/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <RegistrationForm />
+  </div>
+</template>
+
+<script setup>
+import RegistrationForm from './components/RegistrationForm.vue'
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+  background-color: #f4f4f9;
+}
+</style>
