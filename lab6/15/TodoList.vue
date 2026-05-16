6358d42fd7073ac098000ce869b78989e37bbb85
feldushka <mgjmb6w2jm@privaterelay.appleid.com>
2026-05-16 16:08:49 +0300

    Create TodoList.vue

---
+++ lab6/15/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Список задач (Pinia)</h1>
+    <router-link to="/add">Добавить новую задачу</router-link>
+    <ul>
+      <li v-for="task in store.tasks" :key="task.id">
+        <router-link :to="'/task/' + task.id">{{ task.text }}</router-link>
+        [{{ task.completed ? 'Выполнено' : 'В работе' }}]
+        | <router-link :to="'/task/' + task.id + '/complete'">Изменить статус</router-link>
+        | <router-link :to="'/task/' + task.id + '/delete'">Удалить</router-link>
+      </li>
+    </ul>
+    <hr>
+    <router-link to="/about">О нас</router-link>
+  </div>
+</template>
+
+<script setup>
+import { useTodoStore } from '../stores/todoStore'
+
+const store = useTodoStore()
+</script>
