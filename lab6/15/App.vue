7e97e46d38a386ec4fbba27d2f917f8812ddead3
feldushka <mgjmb6w2jm@privaterelay.appleid.com>
2026-05-16 16:08:25 +0300

    Create App.vue

---
+++ lab6/15/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <router-link to="/">Список задач</router-link> |
+    <router-link to="/add">Добавить задачу</router-link> |
+    <router-link to="/about">О нас</router-link>
+  </nav>
+  <hr />
+  
+  <router-view></router-view>
+</template>
+
+<style>
+nav {
+  padding: 10px;
+  margin-bottom: 20px;
+  background: #f4f4f4;
+}
+router-link {
+  margin: 0 10px;
+}
+.message {
+  color: green;
+  font-weight: bold;
+}
+</style>
